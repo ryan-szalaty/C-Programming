7d2478f11602253242769fc18fc019a74e9ca458
ryan-szalaty <ryan.szalaty@gmail.com>
2023-08-01 11:42:25 +0900

    Refactored linear for dynamic memory allocation.

--- Week 3/linear.c
+++ Week 3/linear.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    int numbers[6] = {1, 5, 7, 8, 10};
+    int *numbers;
     int counter = 0;
-    for (int i = 0; i < 5; i++) {
-        if (numbers[i] == 8) {
-            printf("Eight was found in the n=%i position.", counter);
+    int length;
+
+    printf("Please input the number of elements (more than 0 and less than 10):");
+    scanf("%d", &length);
+    if (length <= 0 || length > 10) {
+        printf("Invalid input for the number of elements.\n");
+        return 1;
+    }
+
+    numbers = (int*)malloc(length * sizeof(int));
+
+    printf("Please enter the elements one by one:");
+    while (counter < length) {
+        scanf("%d", &numbers[counter]);
+        counter++;
+    }
+
+    int desiredNumber;
+    printf("Tell me what number you would like to find:");
+    scanf("%d", &desiredNumber);
+    for (counter = 0; counter < length; counter++) {
+        if (numbers[counter] == desiredNumber) {
+            printf("%i was found in the n=%i position.\n", desiredNumber, counter);
+            free(numbers);
             return 0;
         }
         else {
             counter++;
         }
     }
-    printf("Eight not found.");
+    printf("%d not found.\n", desiredNumber);
     return 1;
 }
