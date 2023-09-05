54a7669a2b37326deae10de2b81e158c0569f01a
ryan-szalaty <ryan.szalaty@gmail.com>
2023-09-05 10:53:51 +0900

    Restructured layouts.

--- Week 8/app.py
+++ Week 8/app.py
@@ -4,5 +4,9 @@ app = Flask(__name__) #tells Python to make an application
 
 @app.route("/")
 def index():
-    name = request.args.get("name")
-    return render_template("index.html", name=name)
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST"]) #to prevent data from showing in the URL
+def greet():
+    name = request.form.get("name", "world") #request.args.get for GET requests
+    return render_template("greet.html", name=name)
